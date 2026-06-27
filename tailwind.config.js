57a47a4d10114e5eafd75f15393d14393e69bd3f
nabilpwqz <nabilpwqz@gmail.com>
2026-06-27 20:48:11 +0600

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#fff7ed',
+          100: '#ffedd5',
+          500: '#f59e0b',
+          600: '#d97706',
+          700: '#b45309'
+        }
+      }
+    }
+  },
+  plugins: []
+};
